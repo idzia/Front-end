5261e0fe28502ce940ca279f8e5688f0c1fc4ac0
idzia <ania.idzi@gmail.com>
2018-06-29 16:21:58 +0200

    Add form onsubmit events and error div

--- script/behavior.js
+++ script/behavior.js
@@ -1,23 +1,46 @@
 
-function valid() {
+function isEmpty() {
     var name = document.getElementById('fname').value;
     var email = document.getElementById('e-mail').value;
     if (name == "" || email == "") {
-        return false;
-    } else return true;
+        return true;
+    } else return false;
 }
 
 function switchVisibility() {
     var sendButton = document.getElementById('send');
-    var check = valid();
+    var check = isEmpty();
     console.log(check);
     if (check) {
-        sendButton.removeAttribute("disabled");
-    } else 
         sendButton.setAttribute("disabled", "true");
+    } else 
+        sendButton.removeAttribute("disabled");      
 }
 
 function showError(id) {
     document.getElementById(id).style.display = 'block';
 }
 
+function validation() {
+    var name = document.getElementById('fname').value;
+    var email = document.getElementById('e-mail').value;
+    var message = document.getElementById('message').value;
+    var correctName = /^([A-Z]{1}[a-z]{1,31}\s*)+$/;
+    var correctemail = /^[0-9a-zA-Z_.-]+@[0-9a-zA-Z.-]+\.[a-zA-Z]{2,3}$/;
+    var correctmessage = /^\w+/
+    console.log(name);
+    if (correctName.test(name) && correctemail.test(email) && correctmessage.test(message)) {
+        return true;
+    } else
+        showError('fname-error');
+        showError('email-error');
+        showError('message-error');
+        return false;
+
+}
+
+
+// walidacja Imienia: ^([A-Z]{1}[a-z]{1,31}\s*)+$
+// walidacja e-mail: ^[0-9a-zA-Z_.-]+@[0-9a-zA-Z.-]+\.[a-zA-Z]{2,3}$
+// walidacja Login: ^\w+$
+// walidacja hasło: ^(.+){8,20}$
