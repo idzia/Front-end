ed35cf48f3e9b62ef9a6f68f6c9caba13d2dcfdf
idzia <ania.idzi@gmail.com>
2018-09-19 21:21:12 +0200

    Adjustment html, addEventListener to js

--- script/behavior.js
+++ script/behavior.js
@@ -1,43 +1,71 @@
 
 function isEmpty() {
-    var name = document.getElementById('fname').value;
-    var email = document.getElementById('e-mail').value;
+    const name = document.getElementById('fname').value;
+    const email = document.getElementById('e-mail').value;
     if (name == "" || email == "") {
         return true;
-    } else return false;
+    } else {
+        return false;
+    }
 }
 
-function switchVisibility() {
-    var sendButton = document.getElementById('send');
-    var check = isEmpty();
+function toggleDisabled() {
+    const sendButton = document.getElementById('send');
+    const check = isEmpty();
     console.log(check);
     if (check) {
         sendButton.setAttribute("disabled", "true");
-    } else 
-        sendButton.removeAttribute("disabled");      
+    } else {
+        sendButton.removeAttribute("disabled");
+    }
 }
 
 function showError(id) {
     document.getElementById(id).style.display = 'block';
 }
 
-function validation() {
-    var name = document.getElementById('fname').value;
-    var email = document.getElementById('e-mail').value;
-    var message = document.getElementById('message').value;
-    var correctName = /^([A-Z]{1}[a-z]{1,31}\s*)+$/;
-    var correctemail = /^[0-9a-zA-Z_.-]+@[0-9a-zA-Z.-]+\.[a-zA-Z]{2,3}$/;
-    var correctmessage = /^\w+/
+function validation(evt) {
+    const name = document.getElementById('fname').value;
+    const email = document.getElementById('e-mail').value;
+    const message = document.getElementById('message').value;
+    const correctName = /^([A-Z]{1}[a-z]{1,31}\s*)+$/;
+    const correctEmail = /^[0-9a-zA-Z_.-]+@[0-9a-zA-Z.-]+\.[a-zA-Z]{2,3}$/;
+    const correctMessage = /^\w+/
     console.log(name);
-    if (correctName.test(name) && correctemail.test(email) && correctmessage.test(message)) {
-        return true;
-    } else
+    if (correctName.test(name) && correctEmail.test(email) && correctMessage.test(message)) {
+        alert("Congratulations! You send correctly message");
+    } else {
         showError('fname-error');
         showError('email-error');
         showError('message-error');
-        return false;
-
+        evt.preventDefault();
+    }
 }
+
+function userValidation() {
+    const login = document.getElementById('uname').value;
+    const password = document.getElementById('password').value;
+    const correctLogin = "idzia";
+    const correctPassword = "12345";
+    const role = "student";
+    if (login == correctLogin && password == correctPassword) {
+        return role;
+    } else {
+        showError('login-error');
+        return false;
+    }
+}
+
+function init() {
+    document.getElementById("fname").addEventListener('keyup', toggleDisabled);
+    document.getElementById("e-mail").addEventListener('keyup', toggleDisabled);
+    document.getElementById("contact").addEventListener('submit', validation);
+}
+
+init();
+
+
+
 
 
 // walidacja Imienia: ^([A-Z]{1}[a-z]{1,31}\s*)+$
